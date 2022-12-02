94f9ee175c2ae64dddb07e6862223cd691ae548a
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-12-02 10:46:08 +0100

    Add files via upload

--- finance.py
+++ finance.py
@@ -5,19 +5,23 @@ Created on Thu Dec  1 17:21:08 2022
 
 @author: namnguyen
 """
-
+from datetime import datetime
 import yfinance as yf
 import streamlit as st
 import pandas as pd
 
 st.write("""
         #Simple Stock Price App
-        Shown are the stock ***closing price*** and ***volume*** of Google
+        Shown are the stock ***closing price*** and ***volume*** of Google, Apple, Amazon, Twitter, Tesla
         
         """)
         
-tickerSymbol='GOOGL'
-tickerData=yf.Ticker(tickerSymbol)
-tickerDf=tickerData.history(period='1d', start='2020-1-01', end='2022-11-30')
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+tickerSymbol=['GOOGL','AAPL', 'AMZN', 'TWTR', 'TSLA' ]
+
+for k in tickerSymbol:
+
+    tickerData=yf.Ticker(k)
+    tickerDf=tickerData.history(period='1d', start='2022-1-01', end= datetime.now().date())
+    st.write(k)
+    st.line_chart(tickerDf.Close)
+    st.line_chart(tickerDf.Volume)
